f41fc6609a4fa9e0419c1a21d5ab16aec312a4f5
Theo Pearson-Bray <tpb1908dev@gmail.com>
2020-05-04 13:57:10 +0100

    Use daemon threads for dbus and zmq messaging.

--- bar.py
+++ bar.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from typing import Optional, List
-from threading import Thread, Event
+from threading import Thread
+from time import sleep
 import os
 import argparse
 from warnings import warn
@@ -18,6 +19,7 @@ from Xlib import X
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 import dbus.service
+
 
 class ActionInvoker(dbus.service.Object):
     NOTIFICATIONS_DBUS_INTERFACE = 'org.freedesktop.Notifications'
@@ -53,7 +55,6 @@ bar_size = 35  # bar ends up larger than this anyway
 
 # Dock bar implementation modified from https://gist.github.com/johnlane/351adff97df196add08a
 class TestBar(Gtk.Window):
-
     ACTION_PIPE = "/tmp/notif-nagbar-pip.fifo"
 
     def __init__(self, notification: int, summary: str, body: str, application: str, icon_path: Optional[str] = None,
@@ -123,7 +124,6 @@ class TestBar(Gtk.Window):
         self.move(x, y)
         self.resize(width, bar_size)
 
-
         # it must be shown before changing properties
         self.show_all()
         print(f"Window shown. Size {self.get_size()}")
@@ -175,14 +175,14 @@ class TestBar(Gtk.Window):
         self.invoker = ActionInvoker()
 
         print(f"Timeout {self.timeout}")
-        self.timer = Event()
         if self.timeout > 0:
             seconds = timeout / 1000
+
             def sleep_then_quit():
-                self.timer.wait(seconds)
+                sleep(seconds)
                 self.quit()
 
-            Thread(target=sleep_then_quit).start()
+            Thread(target=sleep_then_quit, daemon=True).start()
 
         print("Running main loop")
         self.thread_action_listener()
@@ -232,7 +232,7 @@ class TestBar(Gtk.Window):
             if len(action) != 2:
                 warn(f"Action {action} invalid format. Should be two parts")
             button = Gtk.Button(label=action[1])
-            button.connect("clicked", self.invoke_action)
+            button.connect("clicked", self.button_pressed)
             self.hbox.pack_end(button, False, False, 0)
         if len(self.actions) == 0:
             # Add standard dismissal button
@@ -244,40 +244,48 @@ class TestBar(Gtk.Window):
         print(f"Button {button.get_label()} clicked")
         self.quit()
 
-    def invoke_action(self, button):
-
+    def button_pressed(self, button):
+        print(f"Button pressed. Label {button.get_label()}")
         for action in self.actions:
             if button.get_label() == action[1]:
-                DBusGMainLoop(set_as_default=True)
-                self.invoker.ActionInvoked(self.notification, action[0])
-
-                print(f"Button pressed. Label {button.get_label()}")
-                self.quit()
+                self.invoke_action(action[1])
                 return
 
         # I don't think this will happen, but working off the label strings seems fragile
         warn(f"No matching action found for button label {button.get_label()}. Actions {self.actions}")
 
+    def invoke_action(self, action: int):
+        DBusGMainLoop(set_as_default=True)
+        self.invoker.ActionInvoked(self.notification, action)
+
+        self.quit()
+
     def thread_action_listener(self):
         # Have to you process. GIL will block when we call open in a thread
-        Thread(target=self.consumer).start()
+        Thread(target=self.consumer, daemon=True).start()
 
     def consumer(self):
         context = zmq.Context()
-        # recieve work
         consumer_receiver = context.socket(zmq.PULL)
         consumer_receiver.connect("tcp://127.0.0.1:5557")
         while True:
-            work = consumer_receiver.recv_string()  # utf-8 string
-            print(f"Received work {work}")
+            message = consumer_receiver.recv_string()  # utf-8 string
+            print(f"Received message {message}")
+            if not message.isdigit():
+                warn(f"Received message {message} which is not digit")
+            else:
+                action_idx = int(message)
+                if len(self.actions) > action_idx > 0:
+                    self.invoke_action(self.actions[action_idx][0])
+                else:
+                    warn(f"Action idx {action_idx} invalid for {len(self.actions)} actions")
 
     def quit(self):
-        self.timer.set()
         if self.dismiss:
             print(f"Dismissing notification {self.notification}")
             DBusGMainLoop(set_as_default=True)
             self.invoker.CloseNotification(self.notification)
-            #self.invoker.NotificationClosed(self.notification, 2)  # closed by user
+            # self.invoker.NotificationClosed(self.notification, 2)  # closed by user
         Gtk.main_quit()
 
 
@@ -286,14 +294,16 @@ if __name__ == "__main__":
     urgencies = ["low", "normal", "critical"]
     parser.add_argument('-u', '--urgency', choices=urgencies, help=f"Notification urgency {urgencies}",
                         default=urgencies[1])
-    parser.add_argument('-n', '--notification', type=int, help="Notification id")
+    parser.add_argument('-n', '--notification', type=int, required=True, help="Notification id")
     parser.add_argument('-s', '--summary', type=str, help="Message summary string (may be markup)")
+
 
     # Convert empty strings to None, rather than checking each one later
     def nullable_string(val: str) -> Optional[str]:
         if not val.strip():
             return None
         return val
+
 
     parser.add_argument('-b', '--body', type=nullable_string, help="Message body (may be markup)")
     parser.add_argument('-a', '--application', type=nullable_string)
@@ -312,8 +322,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     print(args)
+    # This can be done with a custom argparse action, but that requires writing an entire class
+    # A custom type requires splitting it ourselves
     for id, name in args.actions:
         if not id.isdigit():
             print(f"Action identifiers must be integers. Found {id} {name}")
             exit(2)
-    TestBar(args.notification, args.summary, args.body, args.application, args.icon, args.timeout, args.actions, args.dismiss)
+    TestBar(args.notification, args.summary, args.body, args.application, args.icon, args.timeout, args.actions,
+            args.dismiss)
